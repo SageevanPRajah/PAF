022d8cc1bd9f3f6faa3b7b6f74e386a962552010
ISURU KULATUNGA <it22218544@my.sliit.lk>
2025-05-07 21:55:34 +0530

    Fix formatting issue in CommentNotFoundException by adding a newline at the end of the file

--- nexus-backend/src/main/java/com/roboticgen/nexus/exception/CommentNotFoundException.java
+++ nexus-backend/src/main/java/com/roboticgen/nexus/exception/CommentNotFoundException.java
@@ -2,6 +2,6 @@ package com.roboticgen.nexus.exception;
 
 public class CommentNotFoundException extends RuntimeException {
     public CommentNotFoundException(Long id) {
-        super("Comment not found: " + id);
-    }
+        super("Comment not found: " + id);
+    }
 }
