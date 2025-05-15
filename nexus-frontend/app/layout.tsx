32a70db75520178c262c17543c440842894c6993
IT22094704 <it22094704@my.sliit.lk>
2025-05-15 11:24:52 +0530

    feat: remove sidebar navigation for a cleaner layout

--- nexus-frontend/app/layout.tsx
+++ nexus-frontend/app/layout.tsx
@@ -26,34 +26,7 @@ export default function RootLayout({
 
         <div className="flex min-h-[calc(100vh-64px)]">
           {/* sidebar */}
-          <aside className="w-64 bg-white border-r">
-            <nav className="mt-8 flex flex-col space-y-1">
-              <Link
-                href="/feed"
-                className="flex items-center px-4 py-2 hover:bg-gray-100"
-              >
-                <House className="mr-3" /> Home
-              </Link>
-              <Link
-                href="/posts"
-                className="flex items-center px-4 py-2 hover:bg-gray-100"
-              >
-                <FileText className="mr-3" /> My Profile
-              </Link>
-              <Link
-                href="/process"
-                className="flex items-center px-4 py-2 hover:bg-gray-100"
-              >
-                <BookOpen className="mr-3" /> My Learnings
-              </Link>
-              <Link
-                href="/courses"
-                className="flex items-center px-4 py-2 hover:bg-gray-100"
-              >
-                <Bookmark className="mr-3" /> My Subscribe
-              </Link>
-            </nav>
-          </aside>
+          
 
           {/* main content */}
           <main className="flex-1 bg-gray-50 p-6">
