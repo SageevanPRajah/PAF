8e18561d3a85bc8b816cf3e871823cc033292b48
IT22094704 <it22094704@my.sliit.lk>
2025-05-15 11:22:23 +0530

    feat: add ChatException class for handling chat-related errors

---
+++ nexus-backend/src/main/java/com/roboticgen/nexus/exception/ChatException.java
@@ -0,0 +1,7 @@
+package com.roboticgen.nexus.exception;
+
+public class ChatException extends RuntimeException {
+    public ChatException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
