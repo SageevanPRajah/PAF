c2116e01654aa68466ea71cd226c14b678d1ba06
GBLiyanage <127918030+gb8222@users.noreply.github.com>
2025-05-15 19:08:30 +0530

    Refactor code structure for improved readability and maintainability

--- nexus-frontend/app/posts/page.tsx
+++ nexus-frontend/app/posts/page.tsx
@@ -1,12 +1,19 @@
 'use client'
 import PostForm from './components/PostForm'
 import PostFeed from './components/PostFeed'
+import Header from '../feed/components/Header'
+import ProcessCount from '../process/components/ProcessCount'
 
 export default function PostsPage() {
   return (
-    <div className="max-w-xl mx-auto">
-      <PostForm />
-      <PostFeed />
+    <div className="">
+      <Header />
+
+      <div className="max-w-xl mx-auto">
+        <PostForm />
+        <ProcessCount />
+        <PostFeed />
+      </div>
     </div>
   )
 }
