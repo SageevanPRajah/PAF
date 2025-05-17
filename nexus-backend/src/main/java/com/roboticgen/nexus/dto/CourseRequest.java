90a476d6f0c594d292f29498a48585a0d0418f0e
dimuthuY <it22122964@my.sliit.lk>
2025-05-17 10:12:03 +0530

    Add modules field to CourseRequest DTO

--- nexus-backend/src/main/java/com/roboticgen/nexus/dto/CourseRequest.java
+++ nexus-backend/src/main/java/com/roboticgen/nexus/dto/CourseRequest.java
@@ -1,10 +1,11 @@
 package com.roboticgen.nexus.dto;
+import java.util.List;
 
 import lombok.Data;
 
 @Data
-public class CourseRequest
- {
+public class CourseRequest {
     private String title;
     private String description;
+    private List<ModuleRequest> modules;
 }
